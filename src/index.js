6430c3f134c772aef9854170cee84da7a4eaed07
minsoo <minsu2344@naver.com>
2023-02-19 17:21:06 +0900

    feat: 로그인 회원가입 리덕스 미들웨어 적용

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from '../node_modules/react-router-dom/dist/index';
-import { createStore } from 'redux';
-import rootReducer from './modules/index';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer, { rootSaga } from './modules/index';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga'
 import { Provider } from '../node_modules/react-redux/es/exports';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
