666e5595873dad86a0c156f5fd86feb91a08d390
minsoo <minsu2344@naver.com>
2023-02-19 18:14:54 +0900

    feat: user 체크 미들웨어 & 회원가입 완료 후 홈 화면으로 navigate

--- src/modules/containers/auth/RegisterForm.js
+++ src/modules/containers/auth/RegisterForm.js
@@ -1,13 +1,19 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AuthForm from "../../../components/auth/AuthForm";
-import { changeField, initializeForm } from "../../auth";
+import { changeField, initializeForm, register } from "../../auth";
+import { check } from "../../user";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterForm() {
   const dispatch = useDispatch();
-  const {form} = useSelector(({auth}) => ({
-    form: auth.register
+  const {form, auth, authError, user} = useSelector(({auth, user}) => ({
+    form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user
   }));
+  const navigate = useNavigate();
 
   // 인풋 변경 이벤트 핸들러
   function onChange(e) {
@@ -24,12 +30,39 @@ export default function RegisterForm() {
   // 폼 등럭 이벤트 핸들러
   function onSubmit(e) {
     e.preventDefault();
-    // 구현 예정
+    const {username, password, passwordConfirm} = form;
+    if(password !== passwordConfirm) {
+      // TODO: 오류처리
+      return;
+    }
+    dispatch(register({username, password}))
   }
 
+  // 컴포넌트가 처음 렌더링 될 때 from을 초기화
   useEffect(() => {
     dispatch(initializeForm('register'))
   }, [dispatch])
+
+  // 회원가입 성공/실패 처리
+  useEffect(() => {
+    if(authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      return;
+    }
+    if(auth) {
+      console.log('회원가입 성공');
+      console.log(auth);
+      dispatch(check())
+    }
+  }, [auth, authError, dispatch]);
+
+  // user 값이 잘 설정되었는지 확인
+  useEffect(() => {
+    if(user) {
+      navigate('/')
+    }
+  }, [user, navigate])
 
   return (
     <AuthForm
