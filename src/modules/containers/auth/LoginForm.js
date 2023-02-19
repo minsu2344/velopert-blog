b731f75896d855bcbd514e3c7bae063acf194d2f
minsoo <minsu2344@naver.com>
2023-02-19 18:22:44 +0900

    feat: 로그인 check

--- src/modules/containers/auth/LoginForm.js
+++ src/modules/containers/auth/LoginForm.js
@@ -1,12 +1,18 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import AuthForm from "../../../components/auth/AuthForm";
-import { changeField, initializeForm } from "../../auth";
+import { changeField, initializeForm, login } from "../../auth";
+import { check } from "../../user";
 
 export default function LoginForm() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const {form} = useSelector(({auth}) => ({
-    form: auth.login
+  const {form, auth, authError, user} = useSelector(({auth, user}) => ({
+    form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user
   }));
   // 인풋 변경 이벤트 핸들러
   function onChange(e) {
@@ -23,13 +29,33 @@ export default function LoginForm() {
   // 폼 등록 이벤트 핸들러
   function onSubmit(e) {
     e.preventDefault();
-    // 구현 예정
+    
+    const {username, password} = form;
+    dispatch(login({username, password}))
   }
 
   // 컴포넌트가 처음 렌더링 될 때 form을 초기화 함
   useEffect(() => {
     dispatch(initializeForm('login'))
   }, [dispatch])
+
+  useEffect(() => {
+    if(authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      return;
+    }
+    if(auth) {
+      console.log('로그인 성공');
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch])
+
+  useEffect(() => {
+    if(user) {
+      navigate('/')
+    }
+  }, [user, navigate])
 
   return (
     <AuthForm
